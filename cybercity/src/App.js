afb8ef192ce41d0ae3cfedde537a0f79736b9072
joshue <cristianjoshue@hotmail.com>
2021-02-27 18:29:53 -0600

    Empezando react

---
+++ cybercity/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+// Paginas:
+import Index from './pages/principal/Heades';
+import Nosotros from './pages/Nosotros/Nosotros';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path='/' exact component={Index} />
+          <Route path='/nosotros' exact component={Nosotros} />
+        </Switch>
+      </div>
+    </Router>
+
+  );
+}
+
+export default App;
